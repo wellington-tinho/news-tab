c4503ee076ec567da252f5e8feb3734c0c02e6cd
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-08-26 20:47:51 +0000

    feat: initial page "Em construção"

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from "next/head";
+import styles from "./styles.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <title>Test</title>
+        <title>News Tab | Construção</title>
       </Head>
+      <div className={styles.body}>
+        <main>
+          <h1> News Tab </h1>
+          <span>No momento este projeto esta em construção.</span>
+          <span>Novas atualizações são realizadas aos finais de semana.</span>
+        </main>
+      </div>
     </div>
   );
 }
