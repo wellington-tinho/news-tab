be7666644204deb4984920db299b5026b42ab70a
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-10-07 21:59:39 +0000

    feat: added `api\v1\status` endpoint

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,6 @@
+const baseUrl = "http://localhost:3001";
+
+test("GET - /api/v1/status shold return 200", async () => {
+  const response = await fetch(`${baseUrl}/api/v1/status`);
+  expect(response.status).toBe(200);
+});
