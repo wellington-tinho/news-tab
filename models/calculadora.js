bfd1b9f486cee7fbde6c77ba3905f27e3b6c7106
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-10-07 21:58:49 +0000

    refactor: remove test example

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(numero1, numero2) {
-  if (typeof numero1 !== "number" || typeof numero2 !== "number") {
-    throw new Error("Os dois parâmetros devem ser números.");
-  }
-  return numero1 + numero2;
-}
-
-exports.somar = somar;
--- pages/api/status.js
+++
@@ -1,7 +0,0 @@
-function status(req, res) {
-  res.status(200).json({
-    status: "OK",
-    date: new Date(),
-  });
-}
-export default status;
