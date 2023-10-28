b4b62aebbb96cf416fb3a54908e2e3455ae50a11
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-10-28 23:55:38 +0000

    feat: add  file with database service

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = "http://localhost:3000";
 
 test("GET - /api/v1/status shold return 200", async () => {
   const response = await fetch(`${baseUrl}/api/v1/status`);
