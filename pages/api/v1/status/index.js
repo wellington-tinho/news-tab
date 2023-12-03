4f32c1b7e81e02e7e2b5f09af3db4a512135e2fe
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-12-03 12:20:56 +0000

    feat: add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,11 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  console.log("GET - /api/v1/status");
+
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   res.status(200).json({
     status: "OK",
     date: new Date(),
